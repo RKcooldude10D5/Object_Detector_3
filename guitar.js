ab1638bf985d377f931f157cdf1807d3f15b3168
RKcooldude10D5 <89266597+RKcooldude10D5@users.noreply.github.com>
2022-04-09 20:10:56 +0530

    Add files via upload

---
+++ guitar.js
@@ -0,0 +1,32 @@
+function returntopage(){
+    window.location="index.html";
+}
+var img="";
+status="";
+function preload(){
+    img= loadImage("");
+}
+function setup(){
+    canvas= createCanvas(600, 480);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status=true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
+function draw(){
+     fill("#FF0000");
+     text("Guitar", 210, 210);
+     noFill();
+     stroke("#FF0000");
+     rect(200, 250, 80, 400);
+}
---
+++ main.js
@@ -0,0 +1,6 @@
+function gotocarpage(){
+    window.location="car.html";
+}
+function gotoguitarpage(){
+    window.location="guitar.html";
+}
